f5a5ea3e0774dd992d898636a6dd79b7347292db
pleeko <peterjfranchina@gmail.com>
2020-07-08 21:19:25 -0400

    Updates

---
+++ precache-manifest.de9df4fce55c09b4d817ddb5527b0a64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "015d34e35ae3132e256f0cc82af315c6",
+    "url": "/mtg-data-tools/index.html"
+  },
+  {
+    "revision": "4377ba9548ddccbeef56",
+    "url": "/mtg-data-tools/static/css/main.6be5cc3f.chunk.css"
+  },
+  {
+    "revision": "7994920711c44d912676",
+    "url": "/mtg-data-tools/static/js/2.9611a81c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mtg-data-tools/static/js/2.9611a81c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4377ba9548ddccbeef56",
+    "url": "/mtg-data-tools/static/js/main.2b6e1f6c.chunk.js"
+  },
+  {
+    "revision": "25bd10c53e2603d4af34",
+    "url": "/mtg-data-tools/static/js/runtime-main.31baf935.js"
+  }
+]);
